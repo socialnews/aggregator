f0d5d5590330513b4e41b63b0326930abcc0fe4d
Piet Geursen <pietgeursen@gmail.com>
2015-05-09 12:47:23 +1200

    Add route to handle post to shares. Return test json object.

--- app.es6
+++ app.es6
@@ -1,15 +1,17 @@
 let express = require('express');
+let shares = require('./routes/shares.js');
 let app = express();
 
-app.get('/',  (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/shares', shares ); 
+
 
 let server = app.listen(3000,  () => {
 
-  let host = server.address().address;
-  let port = server.address().port;
+	let host = server.address().address;
+	let port = server.address().port;
+ 
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 'use strict';
 
 var express = require('express');
+var shares = require('./routes/shares.js');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use('/shares', shares);
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 });
