21067c658834b59bc0d9b1ee65b7ee8bc923b89c
Piet Geursen <pietgeursen@gmail.com>
2015-05-13 21:20:50 +1200

    Tests working. Try and deploy to heroku.

--- test/post_shares_spec.js
+++ test/post_shares_spec.js
@@ -1,1 +1,45 @@
-"use strict";
+'use strict';
+
+var request = require('supertest');
+var app = require('../app.js').app;
+var server = require('../app.js').server;
+
+var simple = require('simple-mock');
+var share = require('../db/share.js');
+
+describe('POST /shares', function () {
+
+  var data = {
+    'provider': 'twitter',
+    'link': 'http://somewhere.com',
+    'editor': 'pietgeursen',
+    'created_at': 'now'
+  };
+
+  var postToShares = function postToShares() {
+    return request(app).post('/shares').set('Accept', 'application/json').send(data).expect('Content-Type', /json/);
+  };
+
+  it('responds with json', function (done) {
+    var addSpy = simple.mock(share, 'add').resolveWith([data]);
+    postToShares().expect(200, done);
+  });
+
+  it('accepts a json request with params of provider, link, editor, shared_at', function (done) {
+    var addSpy = simple.mock(share, 'add').resolveWith([data]);
+    postToShares().expect(200, done);
+  });
+
+  it('rejects a json request with incorrect params', function (done) {
+    var data = {
+      'provider': 'twitter' };
+    var addSpy = simple.mock(share, 'add').rejectWith('error');
+    postToShares().expect(400, done);
+  });
+
+  after(function (done) {
+    simple.restore();
+    server.close();
+    done();
+  });
+});
--- test/share_model_spec.js
+++ test/share_model_spec.js
@@ -114,4 +114,3 @@ describe('Shares', function () {
 		});
 	});
 });
-
